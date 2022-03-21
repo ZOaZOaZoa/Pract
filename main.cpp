2d9f8f0b527bd8a5296a540954fd8c37e5ccbfce
ZOaZOaZoa <vbifbk555@gmail.com>
2022-03-21 22:43:57 +0300

    code: added table class for printing. Refactored code

--- main.cpp
+++ main.cpp
@@ -3,12 +3,126 @@
 
 #include "plant.h"
 #include "channel.cpp"
+#include "table.cpp"
 #include <vector>
 #include <math.h>
 #include <string>
-#include <stdlib.h>
 
 //Ввод исходных данных
+void getInitialData(std::vector<channel>& channels, size_t& measureCount);
+//Преобразование double в нужный вид для вывода в таблице
+std::string convertToString(double num);
+//Подготовка вектора всех значений входящих в одну строку с результатами измерений в таблице
+void fillMeasurementsResultsVectorForPrint(const std::vector<channel>& channels, size_t product, std::vector<std::string>& values);
+//Измерение всех каналов
+void measureChannels(Plant plant, std::vector<channel>& channels);
+//Конъюнкция значений проверки качества каждого канала
+bool productPassedQualityCheck(std::vector<channel> channels);
+
+int main()
+{
+    //Объявление переменных и получение их начальных значений
+    Plant plant;
+    plant_init(plant);
+
+    const size_t CHANNELS_COUNT = 3;
+    std::vector<channel> channels(CHANNELS_COUNT);
+    std::vector<bool> channelPassedQualityCheck(channels.size());
+    size_t measureCount;
+
+    getInitialData(channels, measureCount);
+
+    //Создание заголовка таблицы
+    std::vector<std::string> headers = { "Prod. num.",
+                                            "   L1   ", "Good.", "Got Dev. ", "Nom. Dev.",
+                                            "   L2   ", "Good.", "Got Dev. ", "Nom. Dev.",
+                                            "   L3   ", "Good.", "Got Dev. ", "Nom. Dev.",
+                                                "Quality" };
+    Table table = Table(headers);
+    table.generateDelimiter();
+    table.showDelimiter();
+    table.showHeaders();
+    table.showDelimiter();
+
+    //Опрос каналов измерений и обработка полученных результатов
+    size_t defectiveCount = 0;
+    for (size_t product = 0; product < measureCount; product++)
+    {
+        measureChannels(plant, channels);
+
+        //Контроль отклонения от номинального значения по сравнению с номинальным отклонением
+        for (size_t i = 0; i < channels.size(); i++)
+        {
+            channels[i].qualityControl();
+        }
+
+        //Сборка вектора значений для печати их в таблице
+        std::vector<std::string> values(1);
+        fillMeasurementsResultsVectorForPrint(channels, product, values);
+        table.showRow(values);
+        table.showDelimiter();
+
+        if(!productPassedQualityCheck(channels))
+        {
+            defectiveCount++;
+        }
+    }
+
+    //Расчёт процента деффекта
+    std::cout << "Defective: " << defectiveCount << " | ";
+    std::cout << (static_cast<double>(defectiveCount) / measureCount) * 100 << "%\n";
+
+    return 0;
+}
+
+void fillMeasurementsResultsVectorForPrint(const std::vector<channel>& channels, size_t product, std::vector<std::string>& values)
+{
+    values[0] = std::to_string(product + 1);
+    for(size_t i = 0; i < channels.size(); i++)
+    {
+        std::string passedQualityCheck = (channels[i].passedQualityCheck ? "+  " : "-  ");
+        std::string deviation = convertToString(abs(channels[i].lastMeasuredValue - channels[i].nominalValue));
+
+        values.push_back(convertToString( channels[i].lastMeasuredValue ));
+        values.push_back(passedQualityCheck);
+        values.push_back(deviation);
+        values.push_back(convertToString( channels[i].normalDeviation ));
+    }
+    std::string passedTesting = productPassedQualityCheck(channels) ? "+   " : "-   ";
+    values.push_back(passedTesting);
+}
+
+std::string convertToString(double num)
+{
+    num = round(num * 100) / 100;
+    std::string str = std::to_string(num);
+    str.erase( str.find_last_not_of('0') + 1, std::string::npos );
+    if(str.back() == '.')
+    {
+        str.pop_back();
+    }
+
+    return str;
+}
+
+bool productPassedQualityCheck(std::vector<channel> channels)
+{
+    bool result = 1;
+    for (struct channel chan : channels)
+    {
+        result = result && chan.passedQualityCheck;
+    }
+    return result;
+}
+
+void measureChannels(Plant plant, std::vector<channel>& channels)
+{
+    for (size_t i = 0; i < channels.size(); i++)
+    {
+        channels[i].lastMeasuredValue = plant_measure(channels[i].chanNum, plant);
+    }
+}
+
 void getInitialData(std::vector<channel>& channels, size_t& measureCount)
 {
     //Переменная для проверкри правильности ввода
@@ -82,102 +196,4 @@ void getInitialData(std::vector<channel>& channels, size_t& measureCount)
             }
         }
     }
-
-}
-
-//Измерение всех каналов
-void measureChannels(Plant plant, std::vector<channel>& channels)
-{
-    for (size_t i = 0; i < channels.size(); i++)
-    {
-        channels[i].lastMeasuredValue = plant_measure(channels[i].chanNum, plant);
-    }
-}
-
-//Конъюнкция значений проверки качества каждого канала
-bool productPassedQualityCheck(std::vector<channel> channels)
-{
-    bool result = 1;
-    for (struct channel chan : channels)
-    {
-        result = result && chan.passedQualityCheck;
-    }
-    return result;
-}
-
-
-int main()
-{
-
-    //Объявление переменных и получение их начальных значений
-    Plant plant;
-    plant_init(plant);
-
-    const size_t CHANNELS_COUNT = 3;
-    std::vector<channel> channels(CHANNELS_COUNT);
-    std::vector<bool> channelPassedQualityCheck(channels.size());
-    size_t measureCount;
-
-    getInitialData(channels, measureCount);
-
-    //Составление горизонтального разделителя для вывода таблицы
-    std::string tableDelimiter = "+----------+";
-    for(size_t i = 0; i < channels.size(); i++)
-    {
-        tableDelimiter += "--------+-----+---------+---------+";
-    }
-    tableDelimiter += "-------+";
-    //Получаем вид "+----------+--------+-----+---------+---------+--------+-----+---------+---------+--------+-----+---------+---------+-------+"
-    std::cout << "\n" << tableDelimiter << "\n";
-
-    //Заголовок таблицы
-    std::cout << "|Prod. num.|";
-    for (size_t i = 0; i < channels.size(); i++)
-    {
-        //std::cout << "   L" << i + 1 << "   |Good.|Nom. Dev.|Got Dev. |";
-          std::cout << "   L" << i + 1 << "   |Good.|Got Dev. |Nom. Dev.|";
-    }
-    std::cout << "Quality|";
-    //Получаем вид "|Prod. num.|   L1   |Good.|Nom. Dev.|Got Dev. |   L2   |Good.|Nom. Dev.|Got Dev. |   L3   |Good.|Nom. Dev.|Got Dev. |Quality|"
-    std::cout << "\n" << tableDelimiter << "\n";
-
-    //Опрос каналов измерений и обработка полученных результатов
-    size_t defectiveCount = 0;
-    for (size_t product = 0; product < measureCount; product++)
-    {
-        measureChannels(plant, channels);
-
-        //Контроль отклонения от номинального значения по сравнению с номинальным отклонением
-        for (size_t i = 0; i < channels.size(); i++)
-        {
-            channels[i].qualityControl();
-        }
-
-        //Вывод полученных измерений
-        std::cout << "|" << std::setw(10) << product + 1;
-        for (size_t i = 0; i < channels.size(); i++)
-        {
-            std::string passedQualityCheck = (channels[i].passedQualityCheck ? "+" : "-");
-            std::cout << std::setprecision(3)
-            << "|" << std::setw(8) << channels[i].lastMeasuredValue
-            << "|" << std::setw(3) << passedQualityCheck << "  "
-            << "|" << std::setw(9) << abs(channels[i].lastMeasuredValue - channels[i].nominalValue)
-            << "|" << std::setw(9) << channels[i].normalDeviation;
-        }
-        bool boolPassedTesting = productPassedQualityCheck(channels);
-        std::string passedTesting = (boolPassedTesting ? "+" : "-");
-        std::cout << "|" << std::setw(4) << passedTesting << "   ";
-        std::cout << "|\n" << tableDelimiter << "\n";
-
-        if(!boolPassedTesting)
-        {
-            defectiveCount++;
-        }
-    }
-
-    //Расчёт процента деффекта
-    std::cout << "Defective: " << defectiveCount << " | ";
-    std::cout << (static_cast<double>(defectiveCount) / measureCount) * 100 << "%\n";
-
-    return 0;
 }
