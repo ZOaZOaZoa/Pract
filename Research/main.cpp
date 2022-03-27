ef859caf90a07e84c1452b473181b0f23b76d614
ZOaZOaZoa <vbifbk555@gmail.com>
2022-03-27 23:46:01 +0300

    code.research: changed for both iterating nominal values and deviations

--- Research/main.cpp
+++ Research/main.cpp
@@ -8,71 +8,127 @@
 
 using namespace std;
 
-void getInitialData(struct Channel& channel, size_t& count, double& lowestNom, double& highestNom, double& nominalDeviation);
+void getInitialData(struct Channel& channel, size_t& count, double& lowestNom, double& highestNom, double& lowestDeviation, double& highestDeviation);
 void fillWithValuesInRange(std::vector<double>& arr, double start, double end);
+double computeDeffectivePercent(const std::vector<double>& values, double nominalValue, double nominalDeviation);
 std::string convertToString(double num);
 bool channelMeasureTest(double value, double nominalValue, double nominalDeviation);
 
 int main()
 {
-    Plant plant;
-    plant_init(plant);
-
-    struct Channel channel;
-    size_t measuresCount;
-    double lowestNominalValue;
-    double highestNominalValue;
-    double nominalDeviation;
-
-    getInitialData(channel, measuresCount, lowestNominalValue, highestNominalValue, nominalDeviation);
-
-    std::vector<double> measureResults(measuresCount);
-
-    for(size_t i = 0; i < measureResults.size(); i++)
+    bool repeat;
+    do
     {
-        double result = plant_measure(channel.chanNum, plant);
-        measureResults[i] = result;
-    }
+        Plant plant;
+        plant_init(plant);
 
-    const size_t COUNT_OF_NUMS_IN_RANGE = 15;
+        struct Channel channel;
+        size_t measuresCount;
+        double lowestNominalValue;
+        double highestNominalValue;
+        double lowestNominalDevation;
+        double highestNominalDeviation;
 
-    std::vector<double> nominalValues(COUNT_OF_NUMS_IN_RANGE);
+        getInitialData(channel, measuresCount, lowestNominalValue, highestNominalValue, lowestNominalDevation, highestNominalDeviation);
 
-    fillWithValuesInRange(nominalValues, lowestNominalValue, highestNominalValue);
+        std::vector<double> measureResults(measuresCount);
 
-    std::vector<std::string> headers = { "N\\Nom.Val.", "  Value  " };
-    for(double nomVal : nominalValues)
-    {
-        headers.push_back(convertToString(nomVal));
-    }
-
-    Table table = Table(headers);
-    table.generateDelimiter();
-    table.showDelimiter();
-    table.showHeaders();
-    table.showDelimiter();
-
-    std::vector<double> measuredValues(measuresCount);
-    for(size_t i = 0; i < measuredValues.size(); i++)
-    {
-        channel.measure(plant);
-        measuredValues[i] = channel.lastMeasuredValue;
-    }
-
-    for(size_t i = 0; i < measuredValues.size(); i++)
-    {
-        std::vector<std::string> rowValues = { std::to_string(i + 1), convertToString(measuredValues[i]) };
-        for(double nomVal : nominalValues)
+        for(size_t i = 0; i < measureResults.size(); i++)
         {
-            rowValues.push_back( (channelMeasureTest(measuredValues[i], nomVal, nominalDeviation)) ? "+" : "-" );
+            double result = plant_measure(channel.chanNum, plant);
+            measureResults[i] = result;
         }
 
-        table.showRow(rowValues);
+        const size_t COUNT_OF_NUMS_IN_RANGE = 12;
+
+        std::vector<double> nominalValues(COUNT_OF_NUMS_IN_RANGE);
+        std::vector<double> nominalDeviations(COUNT_OF_NUMS_IN_RANGE);
+
+        fillWithValuesInRange(nominalValues, lowestNominalValue, highestNominalValue);
+        fillWithValuesInRange(nominalDeviations, lowestNominalDevation, highestNominalDeviation);
+
+        std::vector<std::string> headers = { "  N.", "Nom.Val.\\Nom.Dev" };
+        for(double nomDev : nominalDeviations)
+        {
+            std::string deviationHeader = " ";
+            deviationHeader += convertToString(nomDev);
+            deviationHeader += " ";
+            headers.push_back(deviationHeader);
+        }
+
+        Table table = Table(headers);
+        table.generateDelimiter();
+        table.showDelimiter();
+        table.showHeaders();
         table.showDelimiter();
 
-    }
+        std::vector<double> measuredValues(measuresCount);
+        for(size_t i = 0; i < measuredValues.size(); i++)
+        {
+            channel.measure(plant);
+            measuredValues[i] = channel.lastMeasuredValue;
+        }
+
+        for(size_t i = 0; i < nominalValues.size(); i++)
+        {
+            std::vector<std::string> rowValues = { std::to_string(i + 1), convertToString(nominalValues[i]) };
+            for(double nomDev : nominalDeviations)
+            {
+                //rowValues.push_back( (channelMeasureTest(measuredValues[i], nomVal, nominalDeviation)) ? "+" : "-" );
+                double deffectivePercent = computeDeffectivePercent(measuredValues, nominalValues[i], nomDev);
+                rowValues.push_back( convertToString(deffectivePercent) );
+            }
+
+            table.showRow(rowValues);
+            table.showDelimiter();
+
+        }
+
+        repeat = false;
+        bool correctInput = false;
+        while(!correctInput)
+        {
+            std::cerr << "Repeat program? y/n: ";
+            std::string answer;
+            std::cin >> answer;
+            if(answer == "y")
+            {
+                correctInput = true;
+                repeat = true;
+            }
+            else if(answer == "n")
+            {
+                correctInput = true;
+            }
+            else
+            {
+                std::cerr << "Incorrect input\n";
+                correctInput = false;
+            }
+        }
+
+    } while(repeat);
+
 
     return 0;
+}
+
+double computeDeffectivePercent(const std::vector<double>& values, double nominalValue, double nominalDeviation)
+{
+
+    double percent = 0;
+    for(double value : values)
+    {
+        if(!channelMeasureTest(value, nominalValue, nominalDeviation))
+        {
+            percent++;
+        }
+    }
+
+    percent /= values.size();
+    percent *= 100;
+
+    return percent;
 }
 
 bool channelMeasureTest(double value, double nominalValue, double nominalDeviation)
@@ -104,7 +160,7 @@ std::string convertToString(double num)
     return str;
 }
 
-void getInitialData(struct Channel& channel, size_t& count, double& lowestNom, double& highestNom, double& nominalDeviation)
+void getInitialData(struct Channel& channel, size_t& count, double& lowestNom, double& highestNom, double& lowestDeviation, double& highestDeviation)
 {
     std::cerr << "Enter channel to research: ";
     bool correctInput = false;
@@ -160,17 +216,18 @@ void getInitialData(struct Channel& channel, size_t& count, double& lowestNom, d
 
     correctInput = false;
     counter = 0;
-    std::cerr << "Enter nominal deviation: ";
+    std::cerr << "Enter nominal deviation range for research <lowest value> <highest value>: ";
     while(!correctInput && counter < 100)
     {
-        std::cin >> nominalDeviation;
-        if(nominalDeviation >= 0)
+        std::cin >> lowestDeviation;
+        std::cin >> highestDeviation;
+        if(lowestDeviation < highestDeviation)
         {
             correctInput = true;
         }
         else
         {
-            std::cerr << "Enter positive value\n";
+            std::cerr << "Lowest value should be lesser then highest\n";
             counter++;
         }
     }
